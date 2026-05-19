829349e27446315592802e58ff9b367c2ccc2b68
Jan Olav <boj012026@gmail.com>
2026-05-19 10:36:34 +0200

    coociclicker working, but not styled

--- src/components/CookieClicker.jsx
+++ src/components/CookieClicker.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 export default function CookieClicker() {
+  const [cookieAmount, setCookieAmount] = useState(0);
+
+  const cookieClick = () => {
+    setCookieAmount((prev) =>  prev + 1);
+  }
+
   return (
     <>
       <h3>Cookie Clicker</h3>
+      <h4>{cookieAmount}</h4>
+      <img onClick={cookieClick} src="../public/cookie.png" alt="Image of a cartoon chocolate Ciike" />
     </>
   )
 }
