06f459ef6f433bd0a7390c3af8224ffa79d2ea0b
Jan Olav <boj012026@gmail.com>
2026-05-21 09:31:42 +0200

    Works, but no style, and forms doesn't clear

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,18 +1,20 @@
 export default function UserForm({users, setUsers}) {
   const addUser = (e) => {
-    const newUser = {};
     e.preventDefault();
-    newUser.username = e.target.getElementByid("usrName").value;
-    newUser.email = e.target.getElementById("usrEmail").value;
-    
+
+    const usrForm = new FormData(e.target); 
     console.log(e.target);
+    const newUser = {
+      username: usrForm.get("usrName"),
+      email: usrForm.get("usrEmail")
+    };
     setUsers( prevUsers => [...prevUsers, newUser]);
   }
   return(
     <>
       <form onSubmit={addUser}>
-        <input id="usrName" type="text" placeholder="name"/>
-        <input id="usrEmail" type="email" placeholder="email"/ >
+        <input id="usrName" name="usrName" type="text" placeholder="name"/>
+        <input id="usrEmail" name="usrEmail" type="email" placeholder="email"/ >
         <input type="submit"/>
       </form>
     </>
